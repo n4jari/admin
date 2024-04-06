7bb1bc01692ef17edfee05745780099cbd338ca1
najari <amirezanajari@gmail.com>
2024-04-06 14:54:47 +0330

    feat-added dashboard components and add style to dashboard component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Card from "../ui/dashboard/card/page";
+import Chart from "../ui/dashboard/chart/page";
+import Transactions from "../ui/dashboard/transactions/page";
+import RightBar from "../ui/dashboard/rightbar/page";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="flex gap-4">
+      <div className="flex gap-20 flex-col" style={{ flex: 3 }}>
+        <div className="flex justify-between gap-20">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Chart />
+        <Transactions />
+      </div>
+      <div className="flex-1">
+        <RightBar />
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
