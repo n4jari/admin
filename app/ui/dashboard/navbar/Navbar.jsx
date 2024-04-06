45dfb6e76a0a48f727efab08e44d678b57100f12
najari <amirezanajari@gmail.com>
2024-04-06 13:16:44 +0330

    feat-create navbar component

--- app/ui/dashboard/navbar/Navbar.jsx
+++ app/ui/dashboard/navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
+
+import { usePathname } from "next/navigation";
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <div>{pathname.split("/").pop()}</div>
+      <div>
+        <div>
+          <MdSearch />
+          <input type="text" placeholder="Search .." />
+        </div>
+        <div>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
