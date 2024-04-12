171a49065cebb997819311f36b21e34d0b0af161
unknown <n4jari@gmail.com>
2024-04-13 00:44:22 +0330

    completed fetch users from db and show data and configuration imgs in next.config file

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 
 const Users = async () => {
   const users = await fetchUsers();
-  console.log(users);
 
   return (
     <div className="p-4 bg-soft rounded-md">
@@ -30,66 +29,38 @@ const Users = async () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="p-2">
-              <div className="flex items-center gap-4">
-                <Image
-                  className="rounded-full object-cover"
-                  src="/noavatar.png"
-                  width={40}
-                  height={40}
-                  alt=""
-                />
-                Amireza Najari
-              </div>
-            </td>
-            <td>n4jari@gmail.com</td>
-            <td>13.04.2022</td>
-            <td>Admin</td>
-            <td>Active</td>
-            <td>
-              <div className="flex gap-2 py-1 px-2">
-                <Link href="/dashboard/users/fakeUrl">
-                  <button className="py-1 px-2 text-sm font-medium bg-blue-500 hover:bg-blue-600 rounded-md border-none w-fit cursor-pointer">
-                    View
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td className="p-2">
+                <div className="flex items-center gap-4">
+                  <Image
+                    className="rounded-full object-cover"
+                    src={user.img || "/noavatar.png"}
+                    width={40}
+                    height={40}
+                    alt="profile"
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt?.toString().slice(4, 16)}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "Active" : "Passive"}</td>
+              <td>
+                <div className="flex gap-2 py-1 px-2">
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className="py-1 px-2 text-sm font-medium bg-blue-500 hover:bg-blue-600 rounded-md border-none w-fit cursor-pointer">
+                      View
+                    </button>
+                  </Link>
+                  <button className="py-1 px-2 text-sm font-medium bg-red-500 hover:bg-red-600 rounded-md border-none w-fit cursor-pointer">
+                    Delete
                   </button>
-                </Link>
-                <button className="py-1 px-2 text-sm font-medium bg-red-500 hover:bg-red-600 rounded-md border-none w-fit cursor-pointer">
-                  Delete
-                </button>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td className="p-2">
-              <div className="flex items-center gap-4">
-                <Image
-                  className="rounded-full object-cover"
-                  src="/noavatar.png"
-                  width={40}
-                  height={40}
-                  alt=""
-                />
-                Bardia Alipour
-              </div>
-            </td>
-            <td>Bardia@gmail.com</td>
-            <td>11.01.2022</td>
-            <td>Client</td>
-            <td>Passive</td>
-            <td>
-              <div className="flex gap-2 py-1 px-2">
-                <Link href="/dashboard/users/fakeUrl">
-                  <button className="py-1 px-2 text-sm font-medium bg-blue-500 hover:bg-blue-600 rounded-md border-none w-fit cursor-pointer">
-                    View
-                  </button>
-                </Link>
-                <button className="py-1 px-2 text-sm font-medium bg-red-500 hover:bg-red-600 rounded-md border-none w-fit cursor-pointer">
-                  Delete
-                </button>
-              </div>
-            </td>
-          </tr>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
