31c2932440af858f93c1c4b93274cdb19c830aa1
unknown <n4jari@gmail.com>
2024-04-13 01:33:57 +0330

    added debounce to search

--- app/ui/dashboard/search/Search.jsx
+++ app/ui/dashboard/search/Search.jsx
@@ -1,18 +1,19 @@
 "use client";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { MdSearch } from "react-icons/md";
+import { useDebouncedCallback } from "use-debounce";
 
 const Search = ({ placeholder }) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const { replace } = useRouter();
 
-  const handleSearch = (e) => {
+  const handleSearch = useDebouncedCallback((e) => {
     const params = new URLSearchParams(searchParams);
     if (e.target.value) params.set("q", e.target.value);
     else params.delete("q");
     replace(`${pathname}?${params}`);
-  };
+  }, 500);
 
   return (
     <div className="flex items-center gap-1 rounded-md px-2 py-1 bg-active-sidebar">
