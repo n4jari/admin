7f78e572faedd095843981fe7b0e28ab7c24c3f1
najari <amirezanajari@gmail.com>
2024-04-13 13:28:29 +0330

    added local mongodb

--- app/lib/data.js
+++ app/lib/data.js
@@ -5,10 +5,10 @@ export const fetchUsers = async (q) => {
   const regex = new RegExp(q, "i");
   try {
     connectToDB();
-    const users = await User.find({ username: { $regex: regex } });
+    const users = await User.find({ username: { $regex: regex } }).maxTimeMS(30000); // Increase timeout to 20 seconds (20000 milliseconds)
     return users;
   } catch (err) {
-    console.log(err);
+    console.error(err);
     throw new Error("Failed to fetch users!");
   }
 };
--- app/lib/utils.js
+++ app/lib/utils.js
@@ -5,10 +5,11 @@ const connection = {};
 export const connectToDB = async () => {
   try {
     if (connection.isConnected) return;
-    const db = await mongoose.connect(process.env.MONGO);
+    // const db = await mongoose.connect(process.env.MONGO);
+    const db = await mongoose.connect("mongodb://localhost:27017/dashboard");
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
-    console.log(error)
+    console.log(error);
     throw new Error(error);
   }
 };
