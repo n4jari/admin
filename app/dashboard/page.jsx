b4692b7a072827cc82a08e6d1e4817ed4ad2648d
najari <amirezanajari@gmail.com>
2024-04-15 13:44:31 +0330

    responsive notics

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,13 +1,16 @@
-import React from "react";
 import Card from "../ui/dashboard/card/page";
 import Chart from "../ui/dashboard/chart/page";
 import Transactions from "../ui/dashboard/transactions/page";
 import RightBar from "../ui/dashboard/rightbar/page";
+import Notices from "../ui/dashboard/notices/Notices";
 
 const Dashboard = () => {
   return (
     <div className="flex gap-4">
       <div className="flex gap-4 flex-col" style={{ flex: 3 }}>
+        <div className="hidden max-lg:block">
+          <Notices position="static" />
+        </div>
         <div className="flex justify-between gap-4">
           <Card />
           <Card />
@@ -16,7 +19,7 @@ const Dashboard = () => {
         <Transactions />
         <Chart />
       </div>
-      <div className="flex-1">
+      <div className="flex-1 max-lg:hidden">
         <RightBar />
       </div>
     </div>
